edb7c73317e65ede63f6cd9fd8e242338b3f3fda
Luis Alexis Leiva Díaz <luisalexis.leiva@blueit.es>
2024-10-24 11:11:19 +0200

    Module 5 Solution

--- module5-solution/src/private/my-info/my-info.controller.js
+++ module5-solution/src/private/my-info/my-info.controller.js
@@ -4,10 +4,20 @@
     angular.module('private')
     .controller('MyInfoController', MyInfoController);
     
-    MyInfoController.$inject = ['PrivateService'];
-    function MyInfoController(PrivateService) {
+    MyInfoController.$inject = ['PrivateService', 'MenuService'];
+    function MyInfoController(PrivateService, MenuService) {
       var myInfoCtrl = this;
       myInfoCtrl.user = PrivateService.getInfo();
+      myInfoCtrl.description = "";
+      myInfoCtrl.image = "images/menu/" + myInfoCtrl.user.menu + "/" +
+        myInfoCtrl.user.menu + myInfoCtrl.user.favourite + ".jpg";
+      MenuService.getDish(myInfoCtrl.user.menu, myInfoCtrl.user.favourite).
+        then(function(response) {
+          myInfoCtrl.description = response.data.description;
+        }).
+        catch(function(response) {
+          console.log(response);
+        });
     }
     
 })();
